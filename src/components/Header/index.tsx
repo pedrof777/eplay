20765e946d1ff67bffc2e518d2fab81a7adae98d
unknown <pedrohenriquef278@gmail.com>
2025-10-12 22:56:10 -0300

    feat: criando header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { HeaderBar, LinkCart, LinkItem, Links } from './styles'
+
+import logo from '../../assets/images/logo.svg'
+import carrinho from '../../assets/images/carrinho.svg'
+
+const Header = () => (
+  <HeaderBar>
+    <div>
+      <img src={logo} alt="EPLAY" />
+      <nav>
+        <Links>
+          <LinkItem>
+            <a href="#">Categorias</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Promoções</a>
+          </LinkItem>
+        </Links>
+      </nav>
+    </div>
+    <LinkCart>
+      0 - Produtos
+      <img src={carrinho} alt="carrinho" />
+    </LinkCart>
+  </HeaderBar>
+)
+
+export default Header
