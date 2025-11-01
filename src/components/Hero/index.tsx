9962829887cfb79499d722bbafd07fefe7d764c7
unknown <pedrohenriquef278@gmail.com>
2025-10-31 21:33:41 -0300

    feat: Adicionando banner de produtos

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,33 @@
+import Button from '../Button'
+import Tag from '../Tag'
+import { Banner, Infos } from './styles'
+
+import bannerImg from '../../assets/images/fundo_hogwarts.png'
+
+const Hero = () => (
+  <Banner style={{ backgroundImage: `url(${bannerImg})` }}>
+    <div className="container">
+      <div>
+        <Tag>RPG</Tag>
+        <Tag>PS5</Tag>
+      </div>
+
+      <Infos>
+        <h2>Hogwarts Legacy</h2>
+        <p>
+          <span>De R$ 250,00</span>
+          Por R$ 190,00
+        </p>
+        <Button
+          variant="secondary"
+          type="button"
+          title="Clique aqui para adicionar este jogo ao carrinho"
+        >
+          Adicionar ao carrinho
+        </Button>
+      </Infos>
+    </div>
+  </Banner>
+)
+
+export default Hero
---
+++ src/pages/Product/index.tsx
@@ -0,0 +1,10 @@
+import { useParams } from 'react-router-dom'
+import Hero from '../../components/Hero'
+
+const Product = () => {
+  const { id } = useParams()
+
+  return <Hero />
+}
+
+export default Product
